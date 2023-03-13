daef69347f2e0cf1928b83a7fe755dcb7741d1fb
Higor Dias Zuqueto <higordiaszuqueto@gmail.com>
2023-03-13 08:32:14 -0300

    complete

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,14 +3,13 @@ interface ProgressBarProps {
 }
 
 export function ProgressBar(props: ProgressBarProps) {
-
   return (
-    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
-      <div
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+      <div 
         role="progressbar"
-        arial-label="Progresso de hábitos completados nesse dia"
-        aria-valuenow={75}
-        className="h-3 rounded-xl bg-violet-600"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         style={{
           width: `${props.progress}%`
         }}
